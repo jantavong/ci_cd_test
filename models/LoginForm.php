b326af46c1be4ed02328d828fe68af504a51a81d
sorinoi <sorinoi11@gmail.com>
2024-03-03 11:38:42 +0700

    Basic configuration for test ci cd.

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -7,9 +7,6 @@ use yii\base\Model;
 
 /**
  * LoginForm is the model behind the login form.
- *
- * @property-read User|null $user
- *
  */
 class LoginForm extends Model
 {
@@ -55,14 +52,15 @@ class LoginForm extends Model
 
     /**
      * Logs in a user using the provided username and password.
-     * @return bool whether the user is logged in successfully
+     * @return boolean whether the user is logged in successfully
      */
     public function login()
-    {
-        if ($this->validate()) {
+    { 
+        if ($this->validate()) { 
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+        } else {
+            return false;
         }
-        return false;
     }
 
     /**
