5c542218b401647edb69482b08d66bcd1b212e82
sorinoi <sorinoi11@gmail.com>
2024-03-03 11:10:55 +0700

    Added secret key.

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'Vko2GXJbQd3Y72THHrVMHM3uDokXWAvS',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
